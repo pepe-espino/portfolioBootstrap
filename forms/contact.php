6f2f7c97790c474a9f6631bdcabb775960410125
pepe-espino <joseantespinodz@gmail.com>
2025-03-05 20:29:53 +0100

    cambio php

--- forms/contact.php
+++ forms/contact.php
@@ -1,27 +1,23 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-  $name = $_POST['name'];
+  $name = $_POST['nombre'];
   $email = $_POST['email'];
-  $subject = $_POST['subject'];
-  $message = $_POST['message'];
+  $subject = $_POST['asunto'];
+  $message = $_POST['mensaje'];
 
-  // Aquí pones tu correo para recibir los mensajes
   $to = 'jespinodaza@gmail.com';
 
-  // Cabecera para indicar quién envía el correo
   $headers = "From: $email" . "\r\n";
   $headers .= "Reply-To: $email" . "\r\n";
   $headers .= "Content-Type: text/plain; charset=UTF-8";
 
-  // Cuerpo del mensaje
   $body = "Has recibido un nuevo mensaje desde tu portfolio:\n\n";
   $body .= "Nombre: $name\n";
   $body .= "Correo: $email\n";
   $body .= "Asunto: $subject\n";
   $body .= "Mensaje:\n$message\n";
 
-  // Enviar el correo
   if (mail($to, $subject, $body, $headers)) {
     echo 'Mensaje enviado correctamente.';
   } else {
